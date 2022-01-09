7d91a28f44764e435b9ea4d575bdc3e25bc1602d
limon <limon9426@gmail.com>
2022-01-09 21:43:26 +0600

    Project Upload

---
+++ My_Blog_Project/App_Login/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from App_Login import views
+
+
+app_name ='App_Login'
+
+
+urlpatterns = [
+    path('signup/',views.sign_up, name='signup'),
+    path ('login/',views.login_page, name='login'),
+    path('logout/', views.logout_page, name='logout'),
+    path('profile/', views.profile, name='profile'),
+    path('change_profile/', views.user_change, name='user_change'),
+    path('password/', views.pass_change, name='pass_change'),
+    path('add_picture/', views.add_pro_pic, name='add_pro_pic'),
+    path('change-picture/', views.change_pro_pic, name='change_pro_pic'),
+
+]
+
